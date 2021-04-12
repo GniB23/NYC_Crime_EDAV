fa5e58689089732f16476147c934ec16f6fcfe7e
毕营 <bing@biyingdeMacBook-Pro.local>
2021-04-12 11:48:43 +0800

    Update book

--- _common.R
+++ _common.R
@@ -3,3 +3,12 @@ knitr::opts_chunk$set(
   message = FALSE,
   warning = FALSE
 )
+library(tidyverse)
+library(cowplot)
+library(ggplot2)
+library(ggpubr)
+library(forcats)
+library(maps)
+library(gridExtra)
+library(readr)
+library(viridis)
